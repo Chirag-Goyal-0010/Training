b9a539a69036f04ec4f8a67146f721c2d592d9a3
Chirag Goyal <goyalchirag573@gmail.com>
2024-06-18 17:10:44 +0530

    Status of 18th_June

--- Ruby/hello.rb
+++ Ruby/hello.rb
@@ -1,5 +1,51 @@
-print "How old are you ? "
-age = gets.chomp
-print "How tall are you ?"
-height = gets.chomp
-puts " You are #{age} year old and your height is #{height} cms"
+# # Ruby program for calculating the Nth Fibonacci number.
+# def Fibonacci(number)
+ 
+#     # Base case :  when N is less than 2.
+#     if number < 2
+#       number
+#     else
+   
+#       # Recursive call : sum of last two Fibonacci's.
+#       return Fibonacci(number - 1) + Fibonacci(number - 2)
+#     end
+#   end
+   
+# #   puts Fibonacci(2)
+  
+
+# # Ruby program of regular expression 
+
+# a="2mnanc3"
+# b="2km.5"
+# # . literal matches for all character 
+# if(a.match(/\d.....\d/)) 
+# 	puts("match found") 
+# else
+# 	puts("not found") 
+# end
+# # after escaping it, it matches with only '.' literal 
+# if(a.match(/\d\.\d/)) 
+# 	puts("match found") 
+# else
+# 	puts("not found") 
+# end
+
+# if(b.match(/\d\.\d/)) 
+# 	puts("match found") 
+# else
+# 	puts("not found") 
+# end
+
+
+
+
+# Ruby program of sub and gsub method 
+text = "geeks for geeks, is a computer science portal"
+
+# Change "rails" to "Rails" throughout 
+text.gsub!("geeks", "Geeks") 
+
+# Capitalize the word "Rails" throughout 
+text.gsub!(/\bgeeks\b/, "Geeks") 
+puts "#{text}"
